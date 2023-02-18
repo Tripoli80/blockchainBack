53efcb29eb08530f010b589bdfba21d8ad8b9866
Tripoli80 <tripoli80@gmail.com>
2023-02-19 00:13:48 +0200

    first commit

--- src/services/transactionServices.js
+++ src/services/transactionServices.js
@@ -6,19 +6,23 @@ const getQueryTransactions = async ({ filter, search, size, page }) => {
   const total = await Transaction.find({
     [filter]: { $regex: query },
   }).count();
+  
   const transaction = await Transaction.find({
     [filter]: { $regex: query },
   })
-    .sort({ created_at: -1 })
+    .allowDiskUse(true)
+    .sort({ _id: 1 })
     .skip(skip)
     .limit(size);
   return { total, page, result_size: size, result: transaction };
 };
 
 const getTransactions = async ({ size, page }) => {
-  const total = await Transaction.find({}, []).sort({ created_at: -1 }).count();
-  const transaction = await Transaction.find({}, [])
-    .sort({ created_at: -1 })
+  console.log("🚀 ~ file: transactionServices.js:30 ~ size", size)
+  const total = await Transaction.find().allowDiskUse(true).count();
+  const transaction = await Transaction.find()
+    .allowDiskUse(true)
+    .sort({ _id: 1 })
     .skip(size * (page - 1))
     .limit(size);
 
